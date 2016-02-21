0877340aca0751decdcb0a77a7fc0f613047f69e
gree-gorey <ignatyeff.g@yandex.ru>
2016-02-21 18:22:32 +0300

    create a list of non-verb predicates

--- data/create_json.py
+++ data/create_json.py
@@ -3,21 +3,37 @@
 import codecs
 import json
 
-# prep = {}
-# comp = []
-ins = {}
 
-with codecs.open(u'inserted.csv', u'r', u'utf-8') as f:
-    for line in f:
-        line = line.rstrip()
-        words = line.split(u' ')
-        if words[0] in ins:
-            ins[words[0]].append(line)
-        else:
-            ins[words[0]] = [line]
-        # comp.append(line)
-        # prep[line[0]] = line[1].split(u',')
+def create(type_of):
+    var = [u'predicates',
+           u'inserted',
+           u'complimentizers',
+           u'prepositions']
+    result = None
+    with codecs.open(var[type_of] + u'.csv', u'r', u'utf-8') as f:
+        if type_of == 1:
+            result = {}
+            for line in f:
+                line = line.rstrip()
+                words = line.split(u' ')
+                if words[0] in result:
+                    result[words[0]].append(line)
+                else:
+                    result[words[0]] = [line]
+        elif type_of == 2 or type_of == 0:
+            result = []
+            for line in f:
+                line = line.rstrip()
+                result.append(line)
+        elif type_of == 3:
+            result = {}
+            for line in f:
+                line = line.rstrip()
+                result[line[0]] = line[1].split(u',')
+    return var[type_of] + u'.json', result
 
-w = codecs.open(u'inserted.json', u'w', u'utf-8')
-json.dump(ins, w, ensure_ascii=False, indent=2)
+name, result = create(0)
+
+w = codecs.open(name, u'w', u'utf-8')
+json.dump(result, w, ensure_ascii=False, indent=2)
 w.close()
